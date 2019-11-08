2d2200ea3c7e19b1186d606ac0727673bfbf22e6
Jason Fu <jasonfu6699@hotmail.com>
2019-11-07 23:30:21 -0800

    erase secret access keys oops

--- src/config/config.js
+++ src/config/config.js
@@ -5,10 +5,5 @@ module.exports = {
     endpoint: 'http://localhost:8000',
     accessKeyId: 'a',
     secretAccessKey: 'a'
-  },
-  aws_remote_config: {
-    accessKeyId: 'AKIA5OASAHEGDHTR5FWM',
-    secretAccessKey: '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',
-    region: 'us-west-2'
   }
 };
